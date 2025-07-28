9c671239bb508640cc42c87745bc5404d8c3cad7
Yuri Luka <lukayurii.work@gmail.com>
2025-07-28 16:59:39 +0500

    Question Model

--- DevGuide/Models/Question.swift
+++ DevGuide/Models/Question.swift
@@ -9,17 +9,49 @@ struct Question {
     let title: String
     let answer: [Answer]
     
-//    static func getQuestions() -> [Question] {
-//
-//    }
+    static func getQuestions() -> [Question] {
+        [
+            Question(
+                title: "What type of projects are you most interested in?",
+                answer: [
+                    Answer(title: "Developing games", language: .cSharp),
+                    Answer(title: "Scientific computing and analytics", language: .python),
+                    Answer(title: "Web development", language: .javaScript),
+                    Answer(title: "Creating mobile applications", language: .swift)
+                ]
+            ),
+            Question(
+                title: "How important is cross-platform compatibility to you?",
+                answer: [
+                    Answer(title: "Very important, I want to write once and run on all devices", language: .cSharp),
+                    Answer(title: "Not very important, I can write separately for each platform", language: .javaScript)
+                ]
+            ),
+            Question(
+                title: "How much complexity are you willing to handle?",
+                answer: [
+                    Answer(title: " Prefer simple and understandable languages", language: .swift),
+                    Answer(title: "Ready to learn complex concepts and languages with rich functionality", language: .cSharp)
+                ]
+            ),
+            Question(
+                title: "What tools or development environments do you prefer?",
+                answer: [
+                    Answer(title: "Visual Studio, Visual Studio Code", language: .python),
+                    Answer(title: "Unity", language: .cSharp),
+                    Answer(title: "Web browsers and server platforms", language: .javaScript),
+                    Answer(title: "Specialized IDEs for mobile development", language: .swift)
+                ])
+        ]
+    }
 }
 
 struct Answer {
     let title: String
-    let language: LanguageChosen
+    let language: ProgrammingLanguage
 }
 
-enum LanguageChosen: String {
+enum ProgrammingLanguage: String {
     case cSharp = "C#"
     case python = "Python"
     case javaScript = "JavaScript"
@@ -28,13 +60,13 @@ enum LanguageChosen: String {
     var definition: String {
         switch self {
         case .cSharp:
-            return "Вам подойдет С#"
+            return "The most suitable С#"
         case .python:
-            return "Вам подойдет Python"
+            return "The most suitable Python"
         case .javaScript:
-            return "Вам подойдет JavaScript"
+            return "The most suitable JavaScript"
         case .swift:
-            return "Вам подойдет Swift"
+            return "The most suitable Swift"
         }
     }
 }
